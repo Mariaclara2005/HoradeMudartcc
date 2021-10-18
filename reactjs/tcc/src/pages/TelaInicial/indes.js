ebbe15b5bd2e73193dcad0e8e60424aee136f292
M4theus99 <matheus314000@gmail.com>
2021-10-18 11:58:27 -0300

    OK

---
+++ reactjs/tcc/src/pages/TelaInicial/indes.js
@@ -0,0 +1,189 @@
+
+
+export default function TelaInicial () {
+    return(
+
+        <Container>
+
+        <div class="pagina-principal">
+
+            <div class="cabecalho-inicio">
+
+                <div class="cabecalho-img">
+                    <img src="../src/images/logo-claro.png"/>
+                </div>
+                
+
+                <div class="cabecalho-botoes">
+
+                    
+                     <a href=" ">Denúncie</a>
+                     <a href="pg-tiposdeviolencia/index.html">Tipos de violência</a>
+                     <a href=" ">Tipos de assédio</a>
+                     <a href=" ">Mapa das Delegacia</a>
+                     <a href=" ">Autoestima</a>
+                     <a href=" ">Chat</a>
+                     <a href=" ">Cadastro</a>
+
+                                                   
+                </div>
+
+            </div>
+
+            <div class="texto-centro">
+                Central de Atendimento à Mulher – <a href="https://www.gov.br/pt-br/servicos/denunciar-e-buscar-ajuda-a-vitimas-de-violencia-contra-mulheres">Ligue 180 </a> 
+            </div>
+
+            <div class="faixa-meio">
+                Nunca mude por ninguém! Seja você, para você! Sempre!!!
+            </div>
+
+            <div class="faixa-baixo">
+                Este trabalho é dedicado a todas as mulheres, em especial àquelas mulheres que são agredidas todos os dias e ás que não sobreviveram.
+            </div>
+
+        </div>
+        
+        <div class="tipo-de-violencia">
+
+            <div class="faixa2-conteudo">
+
+                <div class="faixa2-texto1">Tipos De Violência</div>
+
+                <div class="faixa2-texto2">Há varios tipos de violência, fique atenta aos sinas!!!</div>
+
+               <div class="botao-faixa2">
+                <button class="faixa2-botao" >Tipos De Violência</button>
+               </div>
+
+            </div>
+
+            <div class="faixa2-img">
+                <img src="../src/images/tipos-de-violencia.png" class="animate__animated animate__bounce" />
+            </div>
+            
+        </div>
+
+        <div class="autoestima">
+
+            <div class="faixa3-image">
+                <img class="img-faixa3" src="../src/images/autoestima.png" alt=""/>
+            </div>
+
+            <div class="faixa3-conteudo">
+
+                <div class="faixa3-texto1">AUTOESTIMA!</div>
+
+                <div class="faixa3-texto2">Ame teu cabelo, teu jeito, a tua essência, VOCÊ É INCRIVEL!!!</div>
+
+                <div class="faixa3-botao">
+                    <button class="botao-faixa3">IR</button>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="tipos-de-assedio">
+
+            <div class="faixa4-conteudo">
+
+                <div class="faixa4-texto1">Tipos de Assédio</div>
+
+                <div class="faixa4-texto2">Não tenha medo, DENUNCIE!!!</div>
+
+                <div class="faixa4-botao">
+                <button class="botao-faixa4" >IR</button>
+                </div>    
+
+            </div>
+
+            <div class="faixa4-image">
+                <img src="../src/images/tipos-de-assedio.png"/>
+            </div>
+
+        </div>
+
+        <div class="pagina-mapa">
+
+            <div class="faixa5-image">
+                <img src="../src/images/pg-inicio-denuncia.png"/>
+            </div>
+
+            <div class="faixa5-conteudo">
+
+                <div class="faixa5-texto">
+                    Mapa das delegacias mais proximas de você.
+                </div>
+
+                <div class="faixa5-botao">
+                <button class="botao-faixa5">Ir ao Mapa</button>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="pagina-chat">
+
+            <div class="faixa6-conteudo">
+
+                <div class="faixa6-texto">Momento de conversar e conhecer guerreiras como você!!!</div>
+
+                <div class="faixa6-botao">
+                    <button class="botao-faixa6">Ir ao Chat</button>
+                </div>
+
+            </div>
+
+            <div class="faixa6-image">
+                <img src="../src/images/pg-inicio-chat.png"/>
+            </div>
+
+        </div>
+        
+        <div class="pagina-denuncia">
+
+            <div class="faixa7-image">
+                <img src="../src/images/pg-inicio-denunciaF.png" alt=""/>
+            </div>
+
+            <div class="faixa7-conteudo">
+
+                <div class="faixa7-texto1">Denuncie!!!</div>
+
+                <div class="faixa7-texto2">Não tenha medo, você não está sozinha!!!</div>
+
+                <div class="faixa7-botao">
+                    <button class="botao-faixa7">Denuncie</button>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="pagina-quem-somos">
+
+            <div class="faixa8-conteudo">
+
+                <div class="faixa8-texto1">QUEM SOMOS?</div>
+
+                <div class="faixa8-texto2">Conheça mais um pouco sobre nós</div>
+
+                <div class="faixa8-botao">
+                    <button class="botao-faixa8">Saiba mais</button>
+                </div>
+
+            </div>
+
+            <div class="faixa8-image">
+                <img src="../src/images/pg-inicio-quemsomos.png"/ >
+            </div>
+
+        </div>
+
+
+        </Container>
+    
+    )
+}
