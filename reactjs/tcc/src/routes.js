4c1bed6cf1de7066debf320a6d8c687adf256e02
Mariaclara2005 <mariaclara34campos@gmail.com>
2021-10-25 11:42:03 -0300

    maria linda passou aqui s278516

--- reactjs/tcc/src/routes.js
+++ reactjs/tcc/src/routes.js
@@ -1,10 +1,10 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import TelaInicio from './pages/TelaInicial'
-//import AutoEstima from './pages/pg-autoestima'
+import AutoEstima from './pages/pg-autoestima'
 import TipoViolencia from './pages/TiposDeViolencia'
 import Chat from './pages/Chat'
-//import Cadastro from './pages/Cadastro'
+import Cadastro from './pages/Cadastro'
 //import CadastroADM from './pages/CadastroADM'
 //import RedefinirSenha from './pages/RedefinirSenha'
 //import Login from './pages/Login'
@@ -28,10 +28,11 @@ export default function Routes() {
         <BrowserRouter>
             <Switch> 
                 <Route path="/" exact={true} component={TelaInicio}/> 
-                 {/* <Route path="/AutoEstima" exact={true} component={AutoEstima}/> */}
+                  <Route path="/autoestima" exact={true} component={AutoEstima}/> 
                 <Route path="/TipoViolencia" exact={true} component={TipoViolencia}/>
                 <Route path="/Chat" exact= {true} component={Chat}/>
-                {/* <Route path="/Cadastro" exact={true} component={Cadastro}/>
+                <Route path="/Cadastro" exact={true} component={Cadastro}/>
+                {/* 
                 <Route path="/CadastroADM" exact={true} component={CadastroADM} />
                 <Route path="/RedefinirSenha" exact={true} component={RedefinirSenha}/>
                 <Route path="/Login" exact={true} component={Login}/>
